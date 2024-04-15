c3e2186562e8c9165d46715535ca795db663391c
Bioleg <info@bioleg.eu>
2024-04-15 20:41:31 +0200

    Innitial Commit 2

---
+++ enc_temp_folder/15cc288c2b9dbac1d430c66c306ef132/Tank.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+
+ATank::ATank() 
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent); //Call the parent version
+
+	// Get the player controller
+	auto playerController = Cast<APlayerController>(GetController());
+
+	// Get the local player enhanced input subsystem
+	auto eiSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer());
+	//Add the input mapping context
+	eiSubsystem->AddMappingContext(inputMapping, 0);
+
+	// Get the EnhancedInputComponent
+	auto playerEIcomponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	//Bind Move() to the mapping
+	//BindAction for enhanced system takes Action, ETriggerEvent, object, and function
+	//ETriggerEvent is an enum, where Triggered means "button is held down".
+	playerEIcomponent->BindAction(inputMoveForward, ETriggerEvent::Triggered, this, &ATank::Move);
+}
+
+void ATank::Move(const FInputActionValue & Value)
+{
+	FVector DeltaLocation = FVector::ZeroVector;
+	float DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+	DeltaLocation.X = Value.Get<float>() * Speed * DeltaTime;
+	AddActorLocalOffset(DeltaLocation);
+}
